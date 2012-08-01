c16553245d1cb187c16a1d870b19d2565a1b263d
Jessie Chang <ubuntu@ip-10-252-134-115.us-west-2.compute.internal>
2012-08-01 21:47:08 +0000

    Added clickable to title and release date (no action yet)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,4 +38,7 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def sort
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rottenpotatoes::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :movies
+  #resources :movies
+  resources :movies do 
+    get 'sort', :on => :collection
+  end
 
   # Sample resource route with options:
   #   resources :products do
